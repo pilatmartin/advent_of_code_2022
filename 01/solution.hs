5d29aa41bd67528f6768632f0a469fb43820cdef
Martin Pilát <8pilatmartin8@gmail.com>
2022-12-01 18:25:06 +0100

    day 1 solution

---
+++ 01/solution.hs
@@ -0,0 +1,17 @@
+import Data.List.Split
+import Data.List
+
+main :: IO()
+main = do
+  s <- readFile "01_data.txt"
+  print $ "First: " ++ (show $ first s)
+  print $ "Second: " ++ (show $ second s)
+
+first :: String -> Int
+first s = maximum $ getElves s
+
+second :: String -> Int
+second s = sum $ take 3 $ sortBy (flip compare) $ getElves s
+
+getElves :: String -> [Int]
+getElves s = map sum $ map (map read) $ splitWhen (=="") $ lines s
